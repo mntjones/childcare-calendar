b9b1c61fd815a09e9a5ab1cf1d5fe05be093ab74
Monica <mntamaru@gmail.com>
2018-07-09 20:39:13 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
+  resources :sitters
+  
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  get "/logout", to: "sessions#destroy"
+
+  root to: "application#home"
 end
