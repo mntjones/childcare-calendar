d8b672e053845bb7e17149bb940545cc617c82f6
Monica <mntamaru@gmail.com>
2018-07-09 22:11:47 -0700

    logic to keep session until logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
+require 'pry'
 class ApplicationController < ActionController::Base
 
 	def home
+
+		if session[:user_id]
+			@user = User.find_by(id: session[:user_id])
+			redirect_to user_path(@user)
+		elsif session[:sitter_id]
+			@sitter = Sitter.find_by(id: session[:sitter_id])
+			redirect_to sitter_path(@sitter)
+		end
 
 	end
 
