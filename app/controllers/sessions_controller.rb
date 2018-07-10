ca3bc02d94166044a0ae73ec5ccddb5114cd05d6
Monica <mntamaru@gmail.com>
2018-07-09 18:14:18 -0700

    session controller updated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+
+	def new
+
+	end
+
+	def create
+		@user = User.find_by(name: params["name"])
+
+    if @user && @user.authenticate(params["password"])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else 
+      redirect_to(controller: 'users', action: 'new')
+    end 
+	end
+
+	def destroy
+		session.delete :user_id
+    redirect_to(controller: 'application', action: 'home')
+	end
+	
 end
